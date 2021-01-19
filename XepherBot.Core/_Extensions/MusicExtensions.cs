2ebbf1b9402b41687bbf7cdbb8dd2ecc1c3b754f
Acha <43567591+achaacha@users.noreply.github.com>
2021-01-19 09:25:31 -0600

    Add files via upload

---
+++ XepherBot.Core/_Extensions/MusicExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Discord;
+
+namespace XepherBot.Extensions
+{
+    public static class MusicExtensions
+    {
+        public static EmbedAuthorBuilder WithMusicIcon(this EmbedAuthorBuilder eab) =>
+            eab.WithIconUrl("https://i.imgur.com/HJdS1ji.gif");
+    }
+}
